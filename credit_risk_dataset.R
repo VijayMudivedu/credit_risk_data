610666f83d9f97e5e81b6b0d7b59177329c8b651
Vijay Mudivedu <Vijay.Mudivedu@iiitb.net>
2018-08-25 20:09:08 +0530

    data preparation model building

--- credit_risk_dataset.R
+++ credit_risk_dataset.R
@@ -4,90 +4,180 @@
 
 
 library(tidyverse)
+#install.packages("VIM")
+library(VIM)
+#install.packages("mice")
+library(mice)
 
-credit_data_df <- read.csv(file = "https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data",
-                           sep = ",",header = FALSE,check.names = TRUE)
+credit_data_df <- read.csv(file = "https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data",sep = ",",header = F,check.names = TRUE,stringsAsFactors = TRUE,na.strings = "?")
+
 
 head(credit_data_df)
 str(credit_data_df)
+summary(credit_data_df)
 
 # check NAs Missing numbers, factors
 # check special characters
-apply(credit_data_df, MARGIN = 2,function(x) sum(x %in% "?"))
-sapply(credit_data_df,summary)
+apply(credit_data_df, MARGIN = 2, function(x) sum(is.na(x)))
 # V1  V2  V3  V4  V5  V6  V7  V8  V9 V10 V11 V12 V13 V14 V15 V16 
 # 12  12   0   6   6   9   9   0   0   0   0   0   0  13   0   0 
 
-
 #checking the correlation between the unknowns
 
+vars_with_NAs <- c("V1","V2","V4","V5","V6","V7","V14") # vars with illegal chars
+vars_with_no_NAs <- c("V3","V8","V9","V10","V12","V13","V15","V16")
+cat_vars <- c("V1","V4","V5","V6","V7","V9","V10","V12","V13","V16")
+int_vars <-c( "V2","V3","V8","V11","V14","V15")
+
+
+
 # V1
-credit_data_df %>% filter(V1 == "?") 
-credit_data_df %>% filter(V1 == "?") %>% summary()
-  # V1  V2  V3     V4  V5  V6  V7  V8  V9 V10 V11 V12 V13 V14 V15 V16 
-  # 6   ? 26.50  2.710  y  p  ?  ? 0.085  f   f   0   f   s 00080    0   -
-  # 7   ? 45.33  1.000  u  g  q  v 0.125  f   f   0   t   g 00263    0   -
-  # 8   ? 20.42  7.500  u  g  k  v 1.500  t   t   1   f   g 00160  234   +
-  # 9   ? 20.08  0.125  u  g  q  v 1.000  f   t   1   f   g 00240  768   +
-  # 10  ? 42.25  1.750  y  p  ?  ? 0.000  f   f   0   t   g 00150    1   -
-# Comments: No correlation between the V1 and V2. V6, V7 are slightly correlated.
+credit_data_df %>% filter(is.na(V1)) 
 
 # V2
-credit_data_df %>% filter(V2 == "?") 
-credit_data_df %>% filter(V2 == "?") %>% summary()
-credit_data_df %>% summary()
+credit_data_df %>% filter(is.na(V2)) 
 
-# imputing the missing values with k-NN
-install.packages("Amelia")
-require(Amelia)
+# V4 -- # V4, V4 V6 V7,V14 missing values are apparently correlatioed. are correlated
+credit_data_df %>% filter(is.na(V4))
 
-# V1,V2,V4,V5,V6,V7,V14
-wip_cc_df <- credit_data_df
-apply(credit_data_df[,c("V1","V2","V4","V5","V6","V7","V14")],
-      MARGIN = 2, function(x) which(x == "?"))
+# Correlation between V6, V7 and V14
+credit_data_df %>% filter(is.na(V6)) 
 
-Amelia::missmap(credit_data_df)
+# checking for patterns V3=V8=V11=0, V13= p, is the correaltion dependency of missing values
+credit_data_df %>% filter((V3==0 & V8 == 0 & V11==0 & V13=="p" ))
 
-V2 <- as.numeric(credit_data_df[!credit_data_df$V2 =="?",]["V2"]) 
-attributes(V2)
-V2$V2 <- as.numeric(V2$V2)
-summary(V2$V2)
-summary(V2$V2[V2$V2 %in% boxplot.stats(x = V2$V2,coef = 1.58)$stats])
-boxplot(as.numeric(V2$V2))
+# detecting the pair-wise correlation of 
+md.pairs(credit_data_df)
 
 
+#V14 missing values is covering the most of the missing values from V4,V5,V6 and V7
 
-# Comments: Missing at Random
+mydata_df <- credit_data_df
 
-# V4 -- # V4, V4 V6 V7 are highly correlated
-credit_data_df %>% filter(V4 == "?")
-credit_data_df %>% filter(V4 == "?") %>% summary()
+# Categorical variable: "V1" "V4"  "V5"  "V6"  "V7"  "V9"  "V10" "V12" "V13" "V16"
+# Continuous Variables: "V2" "V3"  "V8" , Integer: "V11", "V14","V15"
 
-# Correlation between V6, V7 and V13
-credit_data_df %>% filter(V6 == "?")
+# V1,V2,V4,V5,V6,V7,V14 are the missing values
+  
+# imputing using kNN
+my_data_nas_imputed_knn <- VIM::kNN(mydata_df,k=7,imp_var = TRUE)
 
-# correlation between V13 and other variables
-credit_data_df %>% filter(V14 == "?")
-credit_data_df %>% filter(V14 == "?") %>% summary()
+summary(my_data_nas_imputed_knn[vars_with_NAs])
 
-View(credit_data_df)
+my_data_nas_imputed_knn[my_data_nas_imputed_knn[,"V2_imp"] == TRUE,][c("V1","V2","V4","V5","V6","V7","V14")]
 
+
+#imputing with MICE package
+my_data_imputed_rf <-  mice(mydata_df[,!names(mydata_df) %in% "V16" ],
+                            method = "rf",m = 5)
+summary(my_data_imputed_rf)
+
+my_data_imputed_rf$imp
+my_data_imputed_rf$data
+
+mydata_rf_output <- complete(my_data_imputed_rf)
+
+# Final Data frame with imputed output
+credi_card_df_no_nas <- mydata_rf_output
+credi_card_df_no_nas$label <- mydata_df$V16
+str(credi_card_df_no_nas)
 
 # check duplicates
-#  V1    V2    V3 V4 V5 V6 V7 V8  V9  V10 V11 V12 V13   V14     V15 V16
-#   1  b 30.83 0.000  u  g  w  v 1.25  t   t   1   f   g 00202   0   +
-#   2  a 58.67 4.460  u  g  q  h 3.04  t   t   6   f   g 00043 560   +
-#   3  a 24.50 0.500  u  g  q  h 1.50  t   f   0   f   g 00280 824   +
-#   4  b 27.83 1.540  u  g  w  v 3.75  t   t   5   t   g 00100   3   +
-#   5  b 20.17 5.625  u  g  w  v 1.71  t   f   0   f   s 00120   0   +
-#   6  b 32.08 4.000  u  g  m  v 2.50  t   f   0   t   g 00360   0   +
 
-distinct(credit_data_df,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15) %>% nrow()
+distinct(credi_card_df_no_nas,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15) %>% nrow()
 # No duplicates found in the dataset
 
-
-
 # Check outliers
-# Check invalid values
-# check columns
+v2_out <- boxplot.stats(x = credi_card_df_no_nas$V2,coef = 1.58)$out
+v3_out <- boxplot.stats(x = credi_card_df_no_nas$V3,coef = 1.58)$out
+v8_out <- boxplot.stats(x = credi_card_df_no_nas$V8,coef = 1.58)$out
+v14_out <- boxplot.stats(x = credi_card_df_no_nas$V14,coef = 1.58)$out
+v15_out <- boxplot.stats(x = credi_card_df_no_nas$V15,coef = 1.58)$out
+
+# remove outliers
+final_df <- credi_card_df_no_nas[-which(credi_card_df_no_nas$V2 %in% v2_out),]
+final_df <- final_df[-which(credi_card_df_no_nas$V3 %in% v3_out),]
+final_df <- final_df[-which(credi_card_df_no_nas$V8 %in% v8_out),]
+final_df <- final_df[-which(credi_card_df_no_nas$V14 %in% v14_out),]
+final_df <- final_df[-which(credi_card_df_no_nas$V15 %in% v15_out),]
+
+str(final_df)
+
+#--------------------------------------------------------------
+# split the dataset into train, validation and test variables
+#--------------------------------------------------------------
+
+set.seed(100)
+indices <- sample(1:nrow(final_df), 0.6*nrow(final_df))
+
+# training data_frame
+train_df <- final_df[indices,]
+
+# test data frame
+validation_df <- final_df[-indices,]
+
+# test indices
+indices_test <- sample(1:nrow(validation_df),size = 0.5*nrow(validation_df))
+test_df <- validation_df[-indices_test,]
+validation_df <- validation_df[indices_test,]
+
+
+#--------------------------------------------------------------
+# Model Building
+#--------------------------------------------------------------
+
+# Random Forest
+library(caret)
+library(randomForest)
+library(kernlab)
+
+# Using SVM to tune
+
+train_control <- trainControl(method = "repeatedcv",
+                              repeats = 5,
+                              number = 5,
+                              search = "grid",
+                              sampling = "smote"
+                              )
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
